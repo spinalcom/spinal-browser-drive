ef8ad1db4bd06a10b7e5ac88bc701ca31aab84b4
laurent <htd.laurent@gmail.com>
2018-04-20 14:21:37 +0200

    eslint 2

--- app/app.route.js
+++ app/app.route.js
@@ -1,20 +1,20 @@
-angular.module('app.route')
-  .config(["$routeProvider", "$locationProvider",
-    function ($routeProvider, $locationProvider) {
-      $routeProvider
-        .when("/home", {
-          templateUrl: "app/templates/main.html",
-          authenticate: true,
-          controller: 'mainCtrl'
-        })
-        .when("/login", {
-          templateUrl: "app/templates/login.html",
-          authenticate: false,
-          controller: 'loginCtrl'
-        })
+angular.module("app.route").config([
+  "$routeProvider",
+  function($routeProvider) {
+    $routeProvider
+      .when("/home", {
+        templateUrl: "app/templates/main.html",
+        authenticate: true,
+        controller: "mainCtrl"
+      })
+      .when("/login", {
+        templateUrl: "app/templates/login.html",
+        authenticate: false,
+        controller: "loginCtrl"
+      })
 
-        .otherwise({
-          redirectTo: '/home'
-        });
-    }
-  ]);
+      .otherwise({
+        redirectTo: "/home"
+      });
+  }
+]);
