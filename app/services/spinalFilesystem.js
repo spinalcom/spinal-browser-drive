2e7392c69b491f979d8587924cbda2ad7cb9e95b
laurent <htd.laurent@gmail.com>
2020-05-05 18:17:53 +0200

    use upload indeterminate

--- app/services/spinalFilesystem.js
+++ app/services/spinalFilesystem.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2020 SpinalCom - www.spinalcom.com
+ *
+ * This file is part of SpinalCore.
+ *
+ * Please read all of the following terms and conditions
+ * of the Free Software license Agreement ("Agreement")
+ * carefully.
+ *
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ *
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 var angular = require("angular");
 angular.module("app.spinalcom").service("spinalFileSystem", [
   "$q",
@@ -386,10 +410,14 @@ angular.module("app.spinalcom").service("spinalFileSystem", [
     this.handle_FE_progressBar = (model, item) => {
       if (model._info.model_type.get() === "Path") {
         if (model._info.remaining.get() != 0) {
-          let percent =
-            (model._info.to_upload.get() - model._info.remaining.get()) /
-            model._info.to_upload.get();
-          item.upload_pecent = percent * 100;
+          if (model._info.to_upload.get() === model._info.remaining.get()) {
+            item.upload_pecent = 0;
+          } else {
+            let percent =
+              (model._info.to_upload.get() - model._info.remaining.get()) /
+              model._info.to_upload.get();
+            item.upload_pecent = percent * 100;
+          }
         }
       } else if (
         model._info.model_type.get() === "BIM Project" ||
